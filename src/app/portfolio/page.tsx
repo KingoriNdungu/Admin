106a0c9e5604b387952acb9775407d92adc30363
kikkikenaa <kikkindungu@gmail.com>
2024-09-23 22:46:25 +0300

    message to

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,10 @@
+'use client';
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { ChevronDownIcon, ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
 
-
-import axiosApi from './interceptor'
+//comment section
+import axiosApi from '../hooks/interceptor'
 import Modal from './Modal'
 // In page.tsx
 import CategoryModal from './CategoryModal'
