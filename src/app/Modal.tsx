278f927c8266ea45e6b014c6d4779aed4ede500a
andrewgagi <gagidennis@gmail.com>
2024-11-21 13:30:40 +0300

    updates

--- src/app/Modal.tsx
+++ src/app/Modal.tsx
@@ -58,12 +58,18 @@ const Modal: React.FC<modalProps> = ({ open, closeModal,setBlogData,blogData,set
         const formdata = new FormData()
         formdata.append("file", uploadfile)
         if (res) {
-            // toast.success("blog created successfully")
+           
             closeModal()
             const res2 = await axiosApi.post(`/blog/create/image/${res.data._id}`, formdata)
+            toast.success("blog created successfully")
+            setBlogData(null)
 
         }
         console.log(res, res.data, res.data._id)
+    }
+    const handleClose=()=>{
+        setBlogData(null)
+        closeModal()
     }
     return (
         <div className={`${open ? "block " : "hidden"}  z-[999]  bg-[#A0A3A9]/80 h-screen w-full overflow-scroll  fixed inset-x-0 top-0`}>
@@ -73,7 +79,7 @@ const Modal: React.FC<modalProps> = ({ open, closeModal,setBlogData,blogData,set
                 <div className='md:max-w-[70vw] bg-white lg:max-w-[50vw] overflow-y-scroll   space-y-2 w-full cartModal h-full  px-6 py-2'>
 
                     <div className="flex  w-full justify-between items-center">
-                        <button onClick={() => closeModal()} className="z-50 bg-primaryGray p-1 rounded-full">
+                        <button onClick={handleClose } className="z-50 bg-primaryGray p-1 rounded-full">
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
